fd06cf9add74380cb181722dc2907f904b6afb29
Marcio da Silva Santos <marcio.santos@fortbrasil.com.br>
2020-03-02 10:08:33 -0300

    form validation with dinamic modal messages

--- app.js
+++ app.js
@@ -7,6 +7,24 @@ class Despesa {
         this.descricao = descricao
         this.valor = valor
     }
+
+    validarDados() {
+        for (let i in this) {
+            if (this[i] == undefined || this[i] == '' || this[i] == null) {
+                return false
+            }
+        }
+        return true
+    }
+
+    limparForm() {
+        document.getElementById('ano').value = ''
+        document.getElementById('mes').value = ''
+        document.getElementById('dia').value = ''
+        document.getElementById('tipo').value = ''
+        document.getElementById('descricao').value = ''
+        document.getElementById('valor').value = ''
+    }
 }
 
 class BD {
@@ -17,16 +35,16 @@ class BD {
         }
     }
 
-    proximoId(){
+    proximoId() {
         let proximoId = localStorage.getItem('id')
-        return (parseInt(proximoId)+1)
+        return (parseInt(proximoId) + 1)
     }
 
 
     gravar(d) {
         let id = this.proximoId()
-        localStorage.setItem(id,JSON.stringify(d))
-        localStorage.setItem('id',id)
+        localStorage.setItem(id, JSON.stringify(d))
+        localStorage.setItem('id', id)
     }
 }
 
@@ -36,7 +54,7 @@ let bd = new BD()
 function cadastrarDespesa() {
     let ano = document.getElementById('ano')
     let mes = document.getElementById('mes')
-    let dia = document.getElementById('dia')
+    document.getElementById('dia')
     let tipo = document.getElementById('tipo')
     let descricao = document.getElementById('descricao')
     let valor = document.getElementById('valor')
@@ -48,9 +66,28 @@ function cadastrarDespesa() {
         dia.value,
         tipo.value,
         descricao.value,
-        valor.value)
+        valor.value
+    )
 
-    bd.gravar(despesa)
+    if (despesa.validarDados()) {
+        // bd.gravar(despesa)
+        document.getElementById('modal-titulo-div').className = 'modal-header text-success'
+        document.getElementById('modal-titulo').innerHTML = 'Registro Inserido com Sucesso'
+        document.getElementById('modal-conteudo').innerHTML = 'Despesa Cadastrada com Sucesso'
+        document.getElementById('modal-btn').className = 'btn btn-success'
+        document.getElementById('modal-btn').innerHTML = 'Voltar'
+        $('#modalRegistadespesa').modal('show')
+        despesa.limparForm()
+    }
+    else {
+        document.getElementById('modal-titulo-div').className = 'modal-header text-danger'
+        document.getElementById('modal-titulo').innerHTML = 'Erro na Inserção de Dados'
+        document.getElementById('modal-conteudo').innerHTML = 'Erro na Gravação. Verifique se Todos os Campos estão Preenchidos'
+        document.getElementById('modal-btn').className = 'btn btn-danger'
+        document.getElementById('modal-btn').innerHTML = 'Voltar e Corrigir'
+        $('#modalRegistadespesa').modal('show')
+    }
 }
+
 
 
